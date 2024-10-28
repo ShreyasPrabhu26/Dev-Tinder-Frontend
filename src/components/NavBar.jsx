da89532d31632df5a2e9216595a4fd87e959c6d9
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2024-10-28 14:35:31 +0530

    Fix Nav Menu Magrin

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,7 +38,7 @@ const NavBar = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-44 w-52 p-2 shadow">
                             <li>
                                 <Link to={"/profile"} className="justify-between">
                                     Profile
